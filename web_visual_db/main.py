96c7992ed9575f8d4481f635e45953f12200f12b
Laptop <rob@websterguitars.co.uk>
2020-01-01 18:53:57 +0000

    syntax work

--- web_visual_db/main.py
+++ web_visual_db/main.py
@@ -6,12 +6,19 @@ import cgitb
 
 cgitb.enable(format='text')
 
-
+'''
 my_classes = db_oracle.getDBdata('"TYPE", "NAME", "PERIOD", "USE"', '"MY_CLASS"', '"TYPE"')
 my_crops = db_oracle.getDBdata('"CROP", "NAME", "STARTSEASON", "ENDSEASON"', '"MY_CROPS"', '"CROP"')
 
 field_objects = db_oracle.getDBdata('"FIELD_ID", "LOWX", "LOWY", "HIX", "HIY", "AREA", "OWNER", "CROP"', '"MY_FIELDS"', '"FIELD_ID"')
 find_objects = db_oracle.getDBdata('"FIND_ID", "XCOORD", "YCOORD", "TYPE", "DEPTH", "FIELD_NOTES"', '"MY_FINDS"', '"FIND_ID"')
+'''
+
+my_classes = db_oracle.getDBdata("TYPE, NAME, PERIOD, USE", "MY_CLASS", "TYPE")
+my_crops = db_oracle.getDBdata("CROP, NAME, STARTSEASON, ENDSEASON", "MY_CROPS", "CROP")
+
+field_objects = db_oracle.getDBdata("FIELD_ID, LOWX, LOWY, HIX, HIY, AREA, OWNER, CROP", "MY_FIELDS", "FIELD_ID")
+find_objects = db_oracle.getDBdata("FIND_ID, XCOORD, YCOORD, TYPE, DEPTH, FIELD_NOTES", "MY_FINDS", "FIND_ID")
 
 db_oracle.assign_field_colours(field_objects, my_crops)
 db_oracle.assign_find_colours(find_objects, my_classes)
